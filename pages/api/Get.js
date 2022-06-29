ebd19243256c78ff3da9765e99ad186a72632f36
ichiroDaiki <ichirodaiki@protonmail.com>
2022-06-29 16:28:41 -0500

    new api

--- pages/api/Get.js
+++ pages/api/Get.js
@@ -1,5 +1,5 @@
 export const getProjects = async () => {
-  const res = await fetch("https://backendportfolio.onrender.com", {
+  const res = await fetch("https://portfolio-api-icrust.herokuapp.com", {
     method: 'GET',
     headers: [
       ["Content-Type", "application/json"],
